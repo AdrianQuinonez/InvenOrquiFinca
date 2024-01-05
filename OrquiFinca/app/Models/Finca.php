7c27a9dc1356f527bdd9d8c6d6c2102bee9cbe31
AdrianQuinonez <adorquini2017@gmail.com>
2024-01-05 18:47:41 -0500

    feat: :art: creando los modelos

---
+++ OrquiFinca/app/Models/Finca.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $nombre
+ * @property string $proprietario
+ * @property string $compra
+ * @property string $telefono
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ */
+
+class Finca extends Model
+{
+    use HasFactory;
+}
