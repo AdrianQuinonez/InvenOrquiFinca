8632d8fd2adbf72bbcfb647a5381db71d1f4e548
AdrianQuinonez <adorquini2017@gmail.com>
2024-01-05 19:25:10 -0500

    creacion del index y el crear fincas

---
+++ OrquiFinca/resources/views/fincas/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Fincas</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Listado de fincas
+                        <a href="{{ route('fincas.create') }}" class="btn btn-success btn-sm float-end">Nueva finca</a>
+                    </div>
+                    <div class="card-body">
+                        ....
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
