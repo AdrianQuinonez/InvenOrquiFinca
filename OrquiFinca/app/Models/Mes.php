da9bbc8d8fae81c2bcf4c28e802b8d856cdbaba7
AdrianQuinonez <adorquini2017@gmail.com>
2024-01-06 09:55:33 -0500

    migracion de la tabla mes

---
+++ OrquiFinca/app/Models/Mes.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+/**
+ * @property int $id
+ * @property string $nombre
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ */
+class Mes extends Model
+{
+    use HasFactory;
+}
